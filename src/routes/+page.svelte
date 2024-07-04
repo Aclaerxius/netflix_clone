8571d9088387e3562297dd1da4b93f0f138a11c5
Aclaerxius <alicarusu@gmail.com>
2024-07-04 18:37:54 +0300

    Data fetch logic first try

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { options } from './fetch_data';
+import MovieCarousel from './MovieCarousel.svelte';
+import {options} from './fetch.ts';
 </script>
 
 <svelte:head>
@@ -9,12 +10,12 @@
 
 <sectionHero>
 	<div
-		class="hero h-[40vh]"
-		style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
+		class="hero h-[50vh]"
+		style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg); margin-top: -100px"
 	>
 		<div class="hero-overlay bg-gradient-to-r from-black to-transparent opacity-80"></div>
 
-		<div class="hero-content">
+		<div class="hero-content text-left">
 			<div class="max-w-md">
 				<h1 class="text-5xl font-bold">Title</h1>
 				<p class="mb-5">Info</p>
@@ -26,9 +27,10 @@
 	</div>
 </sectionHero>
 
-<h2>New on Netflix</h2>
-<sectionNewOnNetflix>
-	<div class="carousel">
+
+<sectionNewOnNetflix class="z-2" style="margin-top: -100px">
+	<h2>New on Netflix</h2>
+	<div class="carousel w-full max-h-[50vh]">
 		<div class="carousel-item">
 			<img
 				src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
@@ -76,7 +78,7 @@
 
 <h2>Action Movies</h2>
 <sectionActionMovies>
-	<div class="carousel">
+	<div class="carousel max-h-[50vh]">
 		<div class="carousel-item">
 			<img
 				src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
@@ -124,7 +126,7 @@
 
 <h2>Top Rated</h2>
 <sectionTopRated>
-	<div class="carousel">
+	<div class="carousel max-h-[50vh]">
 		<div class="carousel-item">
 			<img
 				src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-	plugins: [require('daisyui'), require('@tailwindcss/typography')]
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    fontFamily: {
+      "sans-serif": ["Roboto", "sans-serif"],
+    },
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
 };
